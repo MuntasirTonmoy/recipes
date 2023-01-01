ee8fc83735494e531b07eee08fe9eb1a07dc1c41
Muntasir Tonmoy <muntasir01521566317@gmail.com>
2023-01-01 18:29:29 +0600

    navigation pc done

--- js/app.js
+++ js/app.js
@@ -1,6 +1,8 @@
+//=======Nav Toggle=====//
 const navBtn = document.querySelector(".nav-btn");
 const navLinks = document.querySelector(".nav-links");
 
 navBtn.addEventListener("click", () => {
   navLinks.classList.toggle("show-links");
 });
+//====================//
