15bcad389c47e161f237f9ab2f67749c46dfe842
Muntasir Tonmoy <muntasir01521566317@gmail.com>
2023-01-01 20:20:21 +0600

    footer done & all the basic page setup done

--- js/app.js
+++ js/app.js
@@ -6,3 +6,8 @@ navBtn.addEventListener("click", () => {
   navLinks.classList.toggle("show-links");
 });
 //====================//
+
+//============ Date ===========//
+const dateElm = document.querySelector(".date");
+const date = new Date().getFullYear();
+dateElm.textContent = date;
